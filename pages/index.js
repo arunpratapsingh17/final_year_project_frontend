d376097ccb95698fcbd5dcc743dbc0323f9425ae
arunpratapsingh17 <218apsj@gmail.com>
2021-06-13 12:55:16 +0530

    Title change

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Alzhemy</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
